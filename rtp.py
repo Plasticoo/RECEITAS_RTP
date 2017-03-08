e42b8aa0270dc01b1b70e66a29930f02f2e9ecab
Romeu Gomes <oh4ianon@hotmail.com>
2017-03-08 16:00:05 +0000

    Improved code.

--- rtp.py
+++ rtp.py
@@ -1,11 +1,14 @@
+import requests
 from urllib import request
 from bs4 import BeautifulSoup
 
 url = "http://media.rtp.pt/praca/rubricas/culinaria/page/"
+url_ = "http://media.rtp.pt/praca/rubricas/culinaria"
 recipe_pages = []
 
-for _p in range(1, 21):
-    _pc = request.urlopen(request.Request(url + str(_p), headers={'User-Agent': 'Mozilla'})).read()
+for _p in range(1, 8):
+    _pc = requests.get(url + str(_p) + "/", headers={'user-agent': 'Mozilla'}).text
+
     _s = BeautifulSoup(_pc, "html.parser")
     _f = _s.find_all("article", class_="col-sm-6", recursive=True)
     for i in _f:
@@ -13,9 +16,9 @@ for _p in range(1, 21):
 
 _k = 0
 for recipe_page in recipe_pages:
-    _pc = request.urlopen(request.Request(recipe_page, headers={'User-Agent': 'Mozilla'})).read()
+    _pc = requests.get(recipe_page, headers={'User-Agent': 'Mozilla'}).text
     _ss = BeautifulSoup(_pc, "html.parser")
-    _t = _ss.find("title").getText().replace('|', '').replace('Praça', '').replace('RTP', '').rstrip().replace(' ', '_')
+    _t = _ss.find("title").getText().replace('|', '').replace('Praça', '').replace('RTP', '').rstrip().replace(' ', '_').replace('/', '')
     _ff = _ss.find("div", class_="entry-content").getText()
     with open("output/" + "rtp_" + str(_k) + "_" + _t + ".txt", "w") as f:
         f.write(_t.replace('_', ' ') + "\n")
